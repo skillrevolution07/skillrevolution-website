0126e1f439a97a4493022032faf129d26459d8e1
skillrevolution07 <skillrevolution07@gmail.com>
2026-06-10 15:22:41 +0530

    Update vite.config.js

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -4,5 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("framer-motion")) return "motion";
+          if (id.includes("swiper")) return "swiper";
+          if (id.includes("react-icons") || id.includes("lucide-react")) return "icons";
+          if (id.includes("react-router-dom")) return "router";
+          if (id.includes("node_modules/react/") || id.includes("node_modules/react-dom/")) return "react";
+        },
+      },
+    },
+  },
 })
